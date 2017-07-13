8377c5db503cca148a23b5939604dffda30021ba
Alex Castro <castro.aco@gmail.com>
2017-07-13 03:23:30 -0300

    Atualização

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Sysproject\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Sysproject\Http\Requests;
+use Sysproject\Http\Controllers\Controller;
+use Sysproject\Client;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(['clients'=>Client::all()]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return response()->json(['client'=>Client::create($request->all())]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json(['client'=>Client::find($id)]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $client = Client::find($id);
+        $status = $client->update($request->all());
+        return response()->json(['status'=>$status]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $client = Client::find($id);
+        $status = $client->delete();
+        return response()->json(['status'=>$status]);
+    }
+}
